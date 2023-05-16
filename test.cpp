8fb64d6ae2cad26cd6c53909381bede078fa65b9
Maffan <maffan@outlook.com>
2023-05-16 11:40:35 +0500

    Test cpp file

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter a number fomr 1-10 ";
+    cin >> number;
+
+    cout << "Your number: " << number << endl;
+
+    return 0;
+}
